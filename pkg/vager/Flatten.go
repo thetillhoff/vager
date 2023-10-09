4e56cb62acab1843b41f34136a02165a42f6d303
Till Hoffmann <till@thetillhoff.de>
2023-10-09 22:04:59 +0200

    restructure flatten

--- pkg/vager/Flatten.go
+++ pkg/vager/Flatten.go
@@ -55,101 +55,130 @@ mainFolderLoop:
 			if err != nil {
 				log.Fatalln("failed opening directory", fullPath, err)
 			}
-			defer subFolder.Close()
-
 			subFileList, err = subFolder.Readdir(0)
 			if err != nil {
 				log.Fatalln("failed reading directory", fullPath, err)
 			}
+			subFolder.Close() // After reading the contents, it can be closed again
 
-			if len(subFileList) == 0 { // If this mainfolder is empty
+			if len(subFileList) == 0 { // If this subFolder is empty
+
 				if verbose {
-					log.Println("Skipping folder", fullPath, "because it's empty")
-				}
-				// TODO delete this folder
-				continue mainFolderLoop // Skip this mainFolder
-			}
-
-			for _, subFolderfile := range subFileList { // Scan mainFolder contents
-				if subFolderfile.IsDir() { // If mainFolder contains a directory
-					if verbose {
-						log.Println("Skipping folder", fullPath, "because it's contains a folder")
-					}
-					continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
-				}
-				if path.Ext(subFolderfile.Name()) != ".mp4" { // If any subFile is not a mp4 file
-					if verbose {
-						log.Println("Skipping folder", fullPath, "because it contains non-mp4 file", subFolderfile.Name())
-					}
-					continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
+					log.Println("Folder", fullPath, "is empty")
 				}
 
-				// TODO Detect resolution from file metadata, not name-suffix
+			} else if len(subFileList) == 1 { // If this subFolder only contains one element
 
-				if name == "" { // If there is no name yet
-					for index, resolution := range resolutionsAsc { // Try to match each resolution
-						if strings.HasSuffix(subFolderfile.Name(), "_"+resolution+".mp4") { // If resolution matches
-							highestResolutionsAscIndex = index                                     // Set this as initial highest
-							name = strings.TrimSuffix(subFolderfile.Name(), "_"+resolution+".mp4") // Set this as name to compare other files against
-						}
-					}
-				} else { // If there is a name already
-					foundResolution := ""
-					for index, resolution := range resolutionsAsc { // Try to match each resolution
-						if strings.HasSuffix(subFolderfile.Name(), "_"+resolution+".mp4") { // If resolution matches
-							foundResolution = resolution
-							if verbose {
-								log.Println("Detected resolution", foundResolution)
-							}
-							if index > highestResolutionsAscIndex { // If found resolution is higher than of the previous files
-								highestResolutionsAscIndex = index // Set this as new highest
-							}
-							if name != strings.TrimSuffix(subFolderfile.Name(), "_"+resolution+".mp4") { // Check name against existing one (from other file/s)
-								if verbose {
-									log.Println("Skipping folder", fullPath, "because not all of the contained mp4 files have the same name (apart from resolution)")
-								}
-								continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
-							} // Else names match -> ok
-						}
-					}
-					if foundResolution == "" { // If none of the resolutions matched
-						if verbose {
-							log.Println("Skipping folder", fullPath, "because not all of the contained mp4 files have a resolution set in their file ending (_<resolution>.mp4)")
-						}
-						continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
-					}
+				if verbose {
+					log.Println("Folder", fullPath, "only contains one element")
 				}
-				if name == "" { // If there is still no name
-					if verbose {
-						log.Println("Skipping folder", fullPath, "because none of the contained mp4 files don't have a _<resolution>.mp4 file ending")
-					}
-					continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
-				}
+
+				name = subFileList[0].Name()
 
 				cleanedName, err = cleanFileName(name)
 				if err != nil {
 					log.Fatalln(err)
 				}
 
-				// TODO Add more cool features :) like
-				// - sorting of specific "terms"
-				// - reading special attributes like publisher, actors, genre from filename
+				// Move file to parent folder
+				subFileList[0].Name()
+				oldLocation := path.Join(fullPath, name)
+				newLocation := path.Join(mainFolderPath, cleanedName)
+				if dryRun {
+					log.Println("Would move '" + oldLocation + "' to '" + newLocation + "'")
+				} else {
+					if verbose {
+						log.Println("Moving '" + oldLocation + "' to '" + newLocation + "'")
+					}
+					err := os.Rename(oldLocation, newLocation)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
 
-			}
-
-			// Move file to parent and rename it
-			oldLocation := path.Join(fullPath, name+"_"+resolutionsAsc[highestResolutionsAscIndex]+".mp4")
-			newLocation := path.Join(mainFolderPath, cleanedName+" - "+resolutionsAsc[highestResolutionsAscIndex]+".mp4")
-			if dryRun {
-				log.Println("Would move '" + oldLocation + "' to '" + newLocation + "'")
 			} else {
-				err := os.Rename(oldLocation, newLocation)
-				if err != nil {
-					log.Fatal(err)
+				for _, subFolderfile := range subFileList { // Scan mainFolder contents
+					if subFolderfile.IsDir() { // If mainFolder contains a directory
+						if verbose {
+							log.Println("Skipping folder", fullPath, "because it's contains a folder")
+						}
+						continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
+					}
+					if path.Ext(subFolderfile.Name()) != ".mp4" { // If any subFile is not a mp4 file
+						if verbose {
+							log.Println("Skipping folder", fullPath, "because it contains non-mp4 file", subFolderfile.Name())
+						}
+						continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
+					}
+
+					// TODO Detect resolution from file metadata, not name-suffix
+
+					if name == "" { // If there is no name yet
+						for index, resolution := range resolutionsAsc { // Try to match each resolution
+							if strings.HasSuffix(subFolderfile.Name(), "_"+resolution+".mp4") { // If resolution matches
+								highestResolutionsAscIndex = index                                     // Set this as initial highest
+								name = strings.TrimSuffix(subFolderfile.Name(), "_"+resolution+".mp4") // Set this as name to compare other files against
+							}
+						}
+					} else { // If there is a name already
+						foundResolution := ""
+						for index, resolution := range resolutionsAsc { // Try to match each resolution
+							if strings.HasSuffix(subFolderfile.Name(), "_"+resolution+".mp4") { // If resolution matches
+								foundResolution = resolution
+								if verbose {
+									log.Println("Detected resolution", foundResolution)
+								}
+								if index > highestResolutionsAscIndex { // If found resolution is higher than of the previous files
+									highestResolutionsAscIndex = index // Set this as new highest
+								}
+								if name != strings.TrimSuffix(subFolderfile.Name(), "_"+resolution+".mp4") { // Check name against existing one (from other file/s)
+									if verbose {
+										log.Println("Skipping folder", fullPath, "because not all of the contained mp4 files have the same name (apart from resolution)")
+									}
+									continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
+								} // Else names match -> ok
+							}
+						}
+						if foundResolution == "" { // If none of the resolutions matched
+							if verbose {
+								log.Println("Skipping folder", fullPath, "because not all of the contained mp4 files have a resolution set in their file ending (_<resolution>.mp4)")
+							}
+							continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
+						}
+					}
+					if name == "" { // If there is still no name
+						if verbose {
+							log.Println("Skipping folder", fullPath, "because none of the contained mp4 files don't have a _<resolution>.mp4 file ending")
+						}
+						continue mainFolderLoop // Stop scanning its contents further and continue with next mainFolder (not subFolder!)
+					}
+
+					cleanedName, err = cleanFileName(name)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					// TODO Add more cool features :) like
+					// - sorting of specific "terms"
+					// - reading special attributes like publisher, actors, genre from filename
+
 				}
-				if verbose {
-					log.Println("Moving '" + oldLocation + "' to '" + newLocation + "'")
+
+				// Move file to parent and rename it
+				oldLocation := path.Join(fullPath, name+"_"+resolutionsAsc[highestResolutionsAscIndex]+".mp4")
+				newLocation := path.Join(mainFolderPath, cleanedName+" - "+resolutionsAsc[highestResolutionsAscIndex]+".mp4")
+				if dryRun {
+					log.Println("Would move '" + oldLocation + "' to '" + newLocation + "'")
+				} else {
+					if verbose {
+						log.Println("Moving '" + oldLocation + "' to '" + newLocation + "'")
+					}
+					err := os.Rename(oldLocation, newLocation)
+					if err != nil {
+						log.Fatal(err)
+					}
 				}
+
 			}
 
 			// TODO Add more cool features :) like
